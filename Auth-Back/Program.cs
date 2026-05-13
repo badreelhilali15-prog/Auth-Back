f1ce71f930c98265c327a63f4729fa4b8a44bcce
badre <badreelhilali15@gmail.com>
2026-05-13 23:24:30 +0000

    Update Docker configuration

--- Auth-Back/Program.cs
+++ Auth-Back/Program.cs
@@ -1,3 +1,100 @@
+//using Auth_Back.Constants;
+//using Auth_Back.Data;
+//using Auth_Back.Models;
+//using Auth_Back.Rsa;
+//using Auth_Back.Services;
+//using Microsoft.AspNetCore.Authentication.JwtBearer;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.IdentityModel.Tokens;
+//using Scalar.AspNetCore;
+//using System.Security.Cryptography;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+
+//builder.Services.AddControllers();
+//// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+//builder.Services.AddOpenApi();
+//// Configuracion de Identity
+//builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+//    .AddEntityFrameworkStores<AppDbContext>()
+//    .AddDefaultTokenProviders();
+//builder.Services.AddScoped<IUserService, UserService>();
+////Conexion a la base 
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//builder.Services.AddScoped<IAuthService, AuthService>();
+//builder.Services.AddAuthentication(options =>
+//{
+//    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//})
+//.AddJwtBearer(options =>
+//{
+//    var rsa = RsaKeyProvider.GetPublicKey();
+//    options.TokenValidationParameters = new TokenValidationParameters
+//    {
+//        ValidateIssuer = true,
+//        ValidateAudience = true,
+//        ValidateLifetime = true,
+//        ValidateIssuerSigningKey = true,
+//        ValidIssuer = "MyApp",
+//        ValidAudience = "MyClient",
+//        IssuerSigningKey = new RsaSecurityKey(rsa)
+//    };
+//});
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("AllowAngular", policy =>
+//    {
+//        policy.WithOrigins("http://localhost:4200")
+//              .AllowAnyHeader()
+//              .AllowAnyMethod();
+//    });
+//});
+
+//app.UseCors("AllowAngular");
+
+
+//var app = builder.Build();
+
+
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.MapScalarApiReference();
+//    app.MapOpenApi();
+//}
+
+//app.UseHttpsRedirection();
+//using (var scope = app.Services.CreateScope())
+//{
+//    var services = scope.ServiceProvider;
+
+//    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+//    string[] roles = { Roles.Admin, Roles.Client, Roles.Freelancer };
+
+//    foreach (var role in roles)
+//    {
+//        var roleExists = await roleManager.RoleExistsAsync(role);
+
+//        if (!roleExists)
+//        {
+//            await roleManager.CreateAsync(new IdentityRole(role));
+//        }
+//    }
+//}
+
+//app.UseAuthentication();
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
 using Auth_Back.Constants;
 using Auth_Back.Data;
 using Auth_Back.Models;
@@ -8,24 +105,26 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
-using System.Security.Cryptography;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-// Configuracion de Identity
+
+// Configuration de Identity
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<AppDbContext>()
     .AddDefaultTokenProviders();
+
 builder.Services.AddScoped<IUserService, UserService>();
-//Conexion a la base 
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+// Connexion à la base de données
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<IAuthService, AuthService>();
+
+// Configuration JWT Authentication
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -34,6 +133,7 @@ builder.Services.AddAuthentication(options =>
 .AddJwtBearer(options =>
 {
     var rsa = RsaKeyProvider.GetPublicKey();
+
     options.TokenValidationParameters = new TokenValidationParameters
     {
         ValidateIssuer = true,
@@ -46,11 +146,18 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
-
+// Configuration CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
-
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -60,9 +167,19 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAngular");
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Seed roles
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
+
+    var db = services.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
 
     var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
 
@@ -78,9 +195,6 @@ using (var scope = app.Services.CreateScope())
         }
     }
 }
-
-app.UseAuthentication();
-app.UseAuthorization();
 
 app.MapControllers();
 
